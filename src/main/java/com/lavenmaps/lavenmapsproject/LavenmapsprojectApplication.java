cd855b6ca4d3e6499ea11d847f158fe8edd9ff23
Carlos Rivas <Cjrivas00@gmail.com>
2024-02-05 11:12:57 -0500

    first commit

---
+++ src/main/java/com/lavenmaps/lavenmapsproject/LavenmapsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.lavenmaps.lavenmapsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavenmapsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavenmapsprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lavenmaps/lavenmapsproject/LavenmapsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavenmaps.lavenmapsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavenmapsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
