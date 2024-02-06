4170ac605bfdfc427a67033d80a1a38ccd95f138
Emmanuel6093 <elorenzo6093@gmail.com>
2024-02-05 20:51:56 -0500

    simple holder endpoints added to controller

--- src/main/java/com/lavenmaps/lavenmapsproject/Controller/MapController.java
+++ src/main/java/com/lavenmaps/lavenmapsproject/Controller/MapController.java
@@ -3,12 +3,23 @@ package com.lavenmaps.lavenmapsproject.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+// Synchronous Handling
+@RestController
 public class MapController {
-    @RequestMapping(value="")
-@ResponseBody
-public String index(){
-    return "Hello World";
+
+@RequestMapping("/")
+public String homePage() {
+    return "Hello Welcome to home page.";
 }
+
+@RequestMapping("/restaurants")
+public String restaurants() {
+    return "Here are some restaurants.";
 }
+
+
+}
+
