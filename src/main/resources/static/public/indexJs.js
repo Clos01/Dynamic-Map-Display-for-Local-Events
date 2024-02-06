0d01ce4ad4db2c209636b6deb59b097645fe6045
Carlos Rivas <Cjrivas00@gmail.com>
2024-02-06 12:30:59 -0500

    intial navbar set up

--- src/main/resources/static/public/indexJs.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    loadHTMLComponent("header", ".Components.header.html");
-});
-
-function loadHTMLComponent(placeholderId, path) {
-    fetch(path).then(response => response.text()).then(data => {
-        document.getElementById(placeholderId).innerHTML = data;
-    });
-}
