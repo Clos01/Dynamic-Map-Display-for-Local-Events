1cb62a2d684531999f9518597538398141c616bf
Emmanuel6093 <elorenzo6093@gmail.com>
2024-02-05 20:02:03 -0500

    setters & getters added

--- src/main/java/com/lavenmaps/lavenmapsproject/Model/Restaurant.java
+++ src/main/java/com/lavenmaps/lavenmapsproject/Model/Restaurant.java
@@ -22,6 +22,28 @@ public Restaurant() {
 
 // getters and setters
 
+public Long getId() {
+    return id; 
+}
 
+public void setId(Long id) {
+    this.id = id;
+}
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name; 
+}
+
+public String getLocation() {
+    return location;
+}
+
+public void setLocation(String location) {
+    this.location = location;
+}
 
 }
