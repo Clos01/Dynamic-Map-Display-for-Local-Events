883db021f07390cac5d379bdd38ba1e054c82749
Carlos Rivas <Cjrivas00@gmail.com>
2024-02-05 23:34:10 -0500

    connected the Controller using thymeleaf html in template directory

--- src/main/java/com/lavenmaps/lavenmapsproject/Controller/MapController.java
+++ src/main/java/com/lavenmaps/lavenmapsproject/Controller/MapController.java
@@ -3,12 +3,19 @@ package com.lavenmaps.lavenmapsproject.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+// Synchronous Handling
+@RestController
 public class MapController {
-    @RequestMapping(value="")
-@ResponseBody
-public String index(){
-    return "Hello World";
+
+@RequestMapping(value = "/index.html")
+public String homePage() {
+    return "index";
+}
+
+@RequestMapping("/restaurants")
+public String restaurants() {
+    return "Here are some restaurants.";
 }
 }
